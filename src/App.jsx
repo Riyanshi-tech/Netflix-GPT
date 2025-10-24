9912e067083c1cdfd29d518659b64b954b717145
Riyanshi1 <riyanshichaudhary449@gmail.com>
2025-10-24 22:49:29 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 
-
+import Body from './Components/Body.jsx'
 function App() {
   
   return (
-    <>
-      <div className="text-3xl font-bold underline bg-amber-300">Hello world!</div>
-      </>
+   <Body />
   )
 }
 
